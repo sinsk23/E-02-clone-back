a0787abb7a75ae1367891ecec07beb734f16e155
김대석 <skywind4225@gmail.com>
2022-08-20 10:47:05 +0900

    파일 생성/controllers, services 제작 완료

---
+++ controllers/user.controllers.js
@@ -0,0 +1,50 @@
+const UserService = require("../services/user.services");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+class UserController {
+    userService = new UserService;
+
+    createUser = async(req, res, next) => {
+        const {userId, email, nickname, password} = req.body;
+
+        const createUserData = await this.userService.createUser(userId, email, nickname, password);
+        
+        if(!createUserData){
+            res.status(400).json({
+                result: false,
+                errormessage: "회원 가입에 실패하였습니다."
+            });
+        };
+
+        res.status(200).json({
+            result: true,
+            message: "회원 가입에 성공하였습니다."
+        });
+    };
+    
+    login = async(userId, password) => {
+        const {userId, password} = req.body;
+
+        const loginUserData = await this.userService.login(userId, password);
+
+        if(!loginUserData){
+            res.status(400).json({
+                result: false,
+                errormessage: "로그인에 실패하였습니다"
+            });
+        };
+
+        let payload = {userId: loginUserData.userId, nickname: loginUserData.nickname};
+        const token = jwt.sign(payload, process.env.secret_key, {expiresIn: "1d"});
+
+        res.status(200).json({
+            result: true,
+            token: token,
+            message: "로그인에 성공하였습니다"
+        });
+    };
+};
+
+module.exports = UserController;
---
+++ services/user.services.js
@@ -0,0 +1,23 @@
+const userRepository = require("../repositories/user.repositories");
+
+class UserService{
+    userRepository = new userRepository();
+
+    createUser = async(userId, email, nickname, password) => {
+        const createUserData = await this.userRepository.createUser(userId, email, nickname, password);
+
+        return{
+            nickname: createUserData.nickname
+        };
+    };
+
+    login = async(userId, password) => {
+        const loginUserData = await this.userRepository.login(userId, password);
+
+        return{
+            userId: loginUserData.userId,
+            nickname: loginUserData.nickname
+        };
+    };
+};
+module.exports = UserService;
