bdcdda3475a08b813769aef1839ecc663d03188e
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-24 18:37:29 +0900

    https 테스트

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const Router = require("./routes/index");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const app = express();
 require("dotenv").config();
 const port = process.env.Port;
 const cors = require("cors");
+
+const options = {
+  ca: fs.readFileSync("/etc/letsencrypt/live/jinyeop.shop/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/jinyeop.shop/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/jinyeop.shop/cert.pem"),
+};
+app.use(express.static("public"));
 
 app.use(
   cors({
@@ -20,8 +30,11 @@ app.get("/", (req, res) => {
   res.status(200).json({ massage: "연동 잘 됨." });
 });
 
-app.listen(port, () => {
-  console.log(port, "포트로 서버가 열렸어요!");
-});
+// app.listen(port, () => {
+//   console.log(port, "포트로 서버가 열렸어요!");
+// });
+
+http.createServer(app).listen(3000);
+https.createServer(options, app).listen(443);
 
 module.exports = app;
