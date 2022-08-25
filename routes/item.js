6f18207ec25fec71935fcf519015b745689435c4
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-25 16:32:40 +0900

    기존 메인 전체데이터 주기

--- routes/item.js
+++ routes/item.js
@@ -5,94 +5,10 @@ const AuthMiddleware = require("../middlewares/auth_middlewares");
 const VerifyMiddleware = require("../middlewares/verify_middlewares");
 const { Op } = require("sequelize");
 
-// // 숙소 모두 보여주기(메인페이지)
-// router.get("/", VerifyMiddleware, async (req, res) => {
-//   try {
-//     const datas = await Item.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["nickname"],
-//         },
-//         {
-//           model: Comment,
-//           attributes: ["star"],
-//         },
-//         {
-//           model: Like,
-//           attributes: ["userkey"],
-//         },
-//       ],
-//       order: [["itemkey", "DESC"]],
-//     });
-
-//     const arr = datas.map((e) => {
-//       return {
-//         itemkey: e.itemkey,
-//         title: e.title,
-//         img: e.img,
-//         category: e.category,
-//         price: e.price,
-//         location: e.location,
-//         star: e.Comments,
-//         like: e.Likes,
-//         auth: e.User.nickname,
-//       };
-//     });
-
-//     const user = res.locals.user;
-//     if (user) {
-//       // 로그인 했을때 메인에 좋아요 게시물 데이터 주기
-//       const likeitems = await Like.findAll({
-//         where: { userkey: user.userkey },
-//       });
-
-//       res.status(200).json({
-//         likes: likeitems.map((i) => {
-//           return i.itemkey;
-//         }),
-//         data: arr,
-//       });
-//       return;
-//     } else {
-//       // 비로그인이면 메인에 좋아요 게시물 데이터 없음
-//       res.status(200).json({
-//         likes: [],
-//         data: arr,
-//       });
-//       return;
-//     }
-//   } catch (err) {
-//     // console.log(err);
-//     res.status(400).json({
-//       result: false,
-//       errormessage: "숙소를 불러오지 못하였습니다",
-//     });
-//     return;
-//   }
-// });
-
-// 네비게이션 테스트
+// 숙소 모두 보여주기(메인페이지)
 router.get("/", VerifyMiddleware, async (req, res) => {
   try {
-    const page = parseInt(req.query.page);
-    // const pageSize = parseInt(req.query.pageSize); // 프론트랑 말해서 5로 고정해도됨
-
-    let start = 0;
-    if (page > 1) {
-      start = (page - 1) * 5;
-    }
-
-    // let where = {};
-    // if (req.query.lastkey) {
-    //   // 맨처음 n개 가져오고 이후 스크롤 할때 lastkey을 넣어서 보내줌(그럼 lastkey보다 작은 itemkey을 조건으로 함)
-    //   const lastkey = parseInt(req.query.lastkey);
-    //   where = { itemkey: { [Op.lt]: lastkey } };
-    // }
-
-    // 처음엔 조건없이 3개 가져오고 이후엔 lastkey 값보다 작은 조건으로 3개 가져옴
     const datas = await Item.findAll({
-      // where,
       include: [
         {
           model: User,
@@ -108,8 +24,6 @@ router.get("/", VerifyMiddleware, async (req, res) => {
         },
       ],
       order: [["itemkey", "DESC"]],
-      limit: 5,
-      offset: start,
     });
 
     const arr = datas.map((e) => {
@@ -139,15 +53,17 @@ router.get("/", VerifyMiddleware, async (req, res) => {
         }),
         data: arr,
       });
+      return;
     } else {
       // 비로그인이면 메인에 좋아요 게시물 데이터 없음
       res.status(200).json({
         likes: [],
         data: arr,
       });
+      return;
     }
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     res.status(400).json({
       result: false,
       errormessage: "숙소를 불러오지 못하였습니다",
@@ -155,6 +71,90 @@ router.get("/", VerifyMiddleware, async (req, res) => {
     return;
   }
 });
+
+// // 네비게이션 테스트
+// router.get("/", VerifyMiddleware, async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page);
+//     // const pageSize = parseInt(req.query.pageSize); // 프론트랑 말해서 5로 고정해도됨
+
+//     let start = 0;
+//     if (page > 1) {
+//       start = (page - 1) * 5;
+//     }
+
+//     // let where = {};
+//     // if (req.query.lastkey) {
+//     //   // 맨처음 n개 가져오고 이후 스크롤 할때 lastkey을 넣어서 보내줌(그럼 lastkey보다 작은 itemkey을 조건으로 함)
+//     //   const lastkey = parseInt(req.query.lastkey);
+//     //   where = { itemkey: { [Op.lt]: lastkey } };
+//     // }
+
+//     // 처음엔 조건없이 3개 가져오고 이후엔 lastkey 값보다 작은 조건으로 3개 가져옴
+//     const datas = await Item.findAll({
+//       // where,
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["nickname"],
+//         },
+//         {
+//           model: Comment,
+//           attributes: ["star"],
+//         },
+//         {
+//           model: Like,
+//           attributes: ["userkey"],
+//         },
+//       ],
+//       order: [["itemkey", "DESC"]],
+//       limit: 5,
+//       offset: start,
+//     });
+
+//     const arr = datas.map((e) => {
+//       return {
+//         itemkey: e.itemkey,
+//         title: e.title,
+//         img: e.img,
+//         category: e.category,
+//         price: e.price,
+//         location: e.location,
+//         star: e.Comments,
+//         like: e.Likes,
+//         auth: e.User.nickname,
+//       };
+//     });
+
+//     const user = res.locals.user;
+//     if (user) {
+//       // 로그인 했을때 메인에 좋아요 게시물 데이터 주기
+//       const likeitems = await Like.findAll({
+//         where: { userkey: user.userkey },
+//       });
+
+//       res.status(200).json({
+//         likes: likeitems.map((i) => {
+//           return i.itemkey;
+//         }),
+//         data: arr,
+//       });
+//     } else {
+//       // 비로그인이면 메인에 좋아요 게시물 데이터 없음
+//       res.status(200).json({
+//         likes: [],
+//         data: arr,
+//       });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json({
+//       result: false,
+//       errormessage: "숙소를 불러오지 못하였습니다",
+//     });
+//     return;
+//   }
+// });
 
 // 숙소 생성하기(미들웨어 적용해야함)
 router.post("/", AuthMiddleware, async (req, res) => {
